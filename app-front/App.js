f0eeec3f0c95a1119cd76357228d15cf28625181
Jose Tovar <86726784+JoseAtovarC@users.noreply.github.com>
2022-07-10 00:44:18 +0200

    feat: diseño de la app (#5)

--- app-front/App.js
+++ app-front/App.js
@@ -5,7 +5,11 @@ import Login from './screens/login';
 import Register from "./screens/register.js";
 import ScreenOne from "./screens/screenOne.js";
 import ScreenTwo from "./screens/screenTwo.js";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { HamburgerIcon, Menu, Pressable, Text } from "native-base";
+import { Image } from "react-native";
+import logo from "./assets/planscapelogo.png"
+import { AUTH_STORAGE_KEY } from "./utils/utils";
 
 
 const stack = createNativeStackNavigator();
@@ -14,10 +18,94 @@ export default function App() {
     <NativeBaseProvider>
       <NavigationContainer>
         <stack.Navigator>
-          <stack.Screen name='login' component={Login} />
-          <stack.Screen name='register' component={Register} />
-          <stack.Screen name='screenOne' component={ScreenOne} />
-          <stack.Screen name='screenTwo' component={ScreenTwo} />
+          <stack.Screen name='login' component={Login}
+            options={{
+              headerStyle: { backgroundColor: "#fde047" },
+              headerTitle: () => (
+                <Image style={{ width: 50, height: 80 }} source={logo} />
+              ),
+
+            }
+            } />
+          <stack.Screen name='register' component={Register}
+            options={{
+              headerStyle: { backgroundColor: "#fde047" },
+              headerTitle: () => (
+                <Image style={{ width: 50, height: 80 }} source={logo} />
+              ),
+            }
+            } />
+          <stack.Screen name='screenOne' component={ScreenOne}
+            options={({ navigation }) => ({
+              headerStyle: { backgroundColor: "#fde047" },
+              headerTitle: () => (
+                <Image style={{ width: 50, height: 80 }} source={logo} />
+              ),
+
+              headerRight: () => (
+                <Menu w="500" h="500" trigger={triggerProps => {
+                  return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+                    <HamburgerIcon style={{ width: 50, height: 80 }} />
+                  </Pressable>;
+                }}>
+
+                  <Menu.Item p="5" onPress={() => navigation.navigate("screenOne")}>
+                    <Text fontSize="2xl" fontWeight="bold">
+                      ScreenOne
+                    </Text>
+                  </Menu.Item>
+                  <Menu.Item p="5" onPress={() => navigation.navigate("screenTwo")}>
+                    <Text fontSize="2xl" fontWeight="bold">
+                      ScreenTwo
+                    </Text>
+                  </Menu.Item>
+                  <Menu.Item p="5" onPress={async () => {
+                    try {
+                      await AsyncStorage.removeItem(AUTH_STORAGE_KEY)
+                      navigation.navigate("login")
+                    } catch (e) {
+                      // remove error
+                    }
+
+
+                  }}>   <Text fontSize="2xl" fontWeight="bold">
+                      Logout
+                    </Text></Menu.Item>
+
+                </Menu>
+              )
+            })
+            } />
+          <stack.Screen name='screenTwo' component={ScreenTwo}
+            options={({ navigation }) => ({
+              headerStyle: { backgroundColor: "#fde047" },
+              headerTitle: () => (
+                <Image style={{ width: 50, height: 80 }} source={logo} />
+              ),
+              headerRight: () => (
+                <Menu w="500" h="500" trigger={triggerProps => {
+                  return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+                    <HamburgerIcon style={{ width: 50, height: 80 }} />
+                  </Pressable>;
+                }}>
+
+                  <Menu.Item onPress={() => navigation.navigate("screenOne")}>Screen One</Menu.Item>
+                  <Menu.Item onPress={() => navigation.navigate("screenTwo")}>Screen Two</Menu.Item>
+                  <Menu.Item onPress={async () => {
+                    try {
+                      await AsyncStorage.removeItem(AUTH_STORAGE_KEY)
+                      navigation.navigate("login")
+                    } catch (e) {
+                      // remove error
+                    }
+
+
+                  }}>Logout</Menu.Item>
+
+                </Menu>
+              )
+            })
+            } />
 
         </stack.Navigator>
       </NavigationContainer>
