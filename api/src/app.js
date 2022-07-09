7c503a6bfef905f6185e67dbed1f7b3e7a820a1d
Jose Tovar <86726784+JoseAtovarC@users.noreply.github.com>
2022-07-09 22:07:44 +0200

    feat: auth  with jwt (#1)

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,7 @@ import userRouter from './routes/user.routes.js'
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { config as dotenv } from 'dotenv'
-
+import { options } from './swaggerOptions.js'
 
 dotenv()
 
@@ -12,10 +12,12 @@ dotenv()
 
 const app = express();
 
+
+const specs = swaggerJSDoc(options)
 app.use(cors())
 app.use(express.json());
 
 app.use('/', userRouter);
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 export default app
